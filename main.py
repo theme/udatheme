d7e375f7cb5d541a0622e260a4f6ff274b891405
theme <xenotheme@gmail.com>
2014-03-15 19:40:00 +0800

    l2 ps1 : rot13

--- main.py
+++ main.py
@@ -19,94 +19,40 @@ import webapp2
 import cgi
 
 form="""
+<h2>Enter some text to ROT13:</h2>
 <form method="post">
-    Your birthday?
-    <br>
-    <label> Month 
-        <input type="text" name="month" value="%(month)s">
-    </label>
-    <label> Day 
-        <input type="text" name="day" value="%(day)s">
-    </label>
-    <label> Year
-        <input type="text" name="year" value="%(year)s">
-    </label>
-
-    <div style="color:red">%(error)s</div>
-    <br>
+    <textarea style="height:100px; width: 400px;" name="text">%(txt)s</textarea>
     <br>
     <input type="submit">
 </form>
 """
 
-months = ['January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December']
-
-month_abbvs = dict((m[:3].lower(),m) for m in months)
-
 def escape_html(s):
     return cgi.escape(s)
 
-def valid_month(month):
-    if month:
-        short_month = month[:3].lower()
-        return month_abbvs.get(short_month)
-
-def valid_day(day):
-    if day and day.isdigit():
-        d = int(day)
-        if 0 < d and d < 32:
-            return d
-
-def valid_year(year):
-    if year and year.isdigit():
-        y = int(year)
-        if 1900 < y and y < 2020:
-            return y
+c1="ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz"
+c2="NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm"
 
 
 class MainPage(webapp2.RequestHandler):
-    def write_form(self, error="", month="", day="", year=""):
-        self.response.out.write(form % {"error": error,
-                                        "month": escape_html(month),
-                                        "day": escape_html(day),
-                                        "year": escape_html(year)})
+    def write_form(self, text=""):
+        self.response.out.write(form % {"txt": escape_html(text)})
 
     def post(self):
-        usr_month = self.request.get("month")
-        usr_day = self.request.get("day")
-        usr_year = self.request.get("year")
+        usr_text = self.request.get("text")
+        text = escape_html(usr_text)
+        rot13_text = ""
 
+        for c in usr_text:
+            if c in c1: 
+                rot13_text += c2[c1.find(c)] 
+            else:
+                rot13_text += c
 
-        month = valid_month(usr_month)
-        day = valid_day(usr_day)
-        year = valid_year(usr_year)
-
-        if not ( month and day and year ):
-            self.write_form("invalid",
-                usr_month,usr_day,usr_year)
-        else:
-            self.redirect("/thanks")
+        self.write_form(rot13_text)
 
     def get(self):
         self.write_form()
 
-class ThanksHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write("Thanks! Valid")
-        
-
-
-app = webapp2.WSGIApplication([	('/', MainPage),
-                                ('/thanks', ThanksHandler),],
+app = webapp2.WSGIApplication([	('/', MainPage),],
 				 				debug=True)
